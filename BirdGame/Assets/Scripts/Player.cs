a0933013c6a7ac95f0dbc3859a842c9daed98e45
ayden <Theownage79@gmail.com>
2024-06-05 12:03:24 -0600

    Gamemanager and game over screen added yipee!

--- BirdGame/Assets/Scripts/Player.cs
+++ BirdGame/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    [SerializeField]
+    private PlayerHUD _playerHUD;
+
+    [SerializeField]
+    private GameOverMenu _gameOverMenu;
+    
+    private GameManager _gameManager;
+
+    private void Awake()
+    {
+        _gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            _gameOverMenu.HandleGameOver();
+            Debug.Log("gameover");
+            
+        }
+    }
 }
