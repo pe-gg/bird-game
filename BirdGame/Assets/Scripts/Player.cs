f8b473bfd4b14893818ccf49a7f1c6e3c24a8744
ayden <Theownage79@gmail.com>
2024-06-06 14:01:48 -0600

    changed player model and other models
    updated player prefab to contain values for going to far out of bounds resulting in a game over

--- BirdGame/Assets/Scripts/Player.cs
+++ BirdGame/Assets/Scripts/Player.cs
@@ -13,10 +13,20 @@ public class Player : MonoBehaviour
     private GameOverMenu _gameOverMenu;
     
     private GameManager _gameManager;
+    private Rigidbody _rb;
+
+    [SerializeField] private float deathPointLow;
+    [SerializeField] private float deathPointHigh;
+
+    [SerializeField] private float _GravityGracePeriod;
 
     private void Awake()
     {
+        
         _gameManager = FindObjectOfType<GameManager>();
+        _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = false;
+        Invoke("Grav", _GravityGracePeriod);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,5 +37,19 @@ public class Player : MonoBehaviour
             Debug.Log("gameover");
             
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.transform.position.y <= deathPointLow || this.transform.position.y >= deathPointHigh )
+        {
+            _gameOverMenu.HandleGameOver();
+            Debug.Log("gameover");
+        }
+    }
+
+    private void Grav()
+    {
+        _rb.useGravity = true;
     }
 }
